4f74f07e2ea29678fcb6f88a86355eef615318b7
benjamin <benjinoble@gmail.com>
2021-10-21 15:44:51 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+
+function saturdayFun (fun = `roller-skate`) {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+saturdayFun(`bathe my dog`);
+
+function mondayWork(activity = `go to the office`) {
+    return `This Monday, I will ${activity}.`;
+}
+
+mondayWork(`work from home`)
+
+// Cheated on the one below.....got me ????
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
